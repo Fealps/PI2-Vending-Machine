c705d082d15208e5348566e2d0d508fe6e8cb154
Martha <marthadntas@gmail.com>
2022-04-19 21:16:09 -0300

    Add missing route to home page

--- tshirt/urls.py
+++ tshirt/urls.py
@@ -3,5 +3,6 @@ from tshirt import views
 
 
 urlpatterns = [
-  path('tshirt/<int:page>', views.index, name='tshirts')
+  path('tshirt/<int:page>', views.index, name='tshirts'),
+  path('', views.index, name='tshirt')
 ]
