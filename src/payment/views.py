09390a3cb9b5b5d265caf84248d48cce14c03ba9
Martha <marthadntas@gmail.com>
2022-05-03 17:31:02 -0300

    Add css style and change behavior thats create qrcode

--- src/payment/views.py
+++ src/payment/views.py
@@ -1,3 +1,4 @@
+import os.path
 from django.shortcuts import render
 from payment.models import Payment
 from tshirt.models import Tshirt
@@ -7,13 +8,17 @@ from pixqrcode import PixQrCode
 def payment(request, id):
   tshirt = Tshirt.objects.get(id=id)
   payment = Payment.objects.create()
-  name = ""
-  mobile = ""
+  name = "Martha Dantas Silva"
+  mobile = "61991302532"
   city = "Brasília"
   amount = str(tshirt.price)
   pix = PixQrCode(name, mobile, city, amount)
-  pix.save_qrcode("/vendingmachine/src/payment/static/img",
-                  f"pix-{payment.id}")
+  if os.path.isfile(f"/ img/pix.png"):
+    path = f"/img/pix.png"
+  else:
+    pix.save_qrcode("/vendingmachine/src/payment/static/img",
+                  f"pix")
+    path = f"/img/pix.png"
 
-  context = {"path": f"/img/pix-{payment.id}.png"}
+  context = {"path": path, "tshirt": tshirt}
   return render(request, 'pix-payment.jinja2', context)
